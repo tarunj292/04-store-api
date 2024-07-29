78a082fa656bb49944cb082270e7301fa43085a7
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-07-30 01:27:06 +0530

    done for express backend

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -30,7 +30,13 @@ const getAllProducts = async (req, res) => {
             regEx,
             (match) => `-${operatorMap[match]}-`
         )
-        console.log(filters)
+        const options = ['price', 'rating']
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-')
+            if (options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) }
+            }
+        })
     }
     let result = Product.find(queryObject)
 
